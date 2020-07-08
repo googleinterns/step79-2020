c1d93280eebc6a04e87bc251b3750adcdc89cf15
Oliver Lance <olly.lance15@gmail.com>
2020-07-08 09:25:58 -0400

    add new current profile page component

---
+++ angularSTEP/src/app/current-profile-page/current-profile-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-profile-page',
+  templateUrl: './current-profile-page.component.html',
+  styleUrls: ['./current-profile-page.component.scss']
+})
+export class CurrentProfilePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
