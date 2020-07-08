9d643f6e85eb7c26baf95dbdcc9660288c4ad793
Oliver Lance <olly.lance15@gmail.com>
2020-07-08 10:56:49 -0400

    add basic ability to click on user widget

--- angularSTEP/src/app/view-profiles/view-profiles.component.ts
+++ angularSTEP/src/app/view-profiles/view-profiles.component.ts
@@ -1,4 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/firestore';
+import {Component, OnInit} from '@angular/core';
+
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
+
+class User {
+  username: string;
+  displayName: string;
+  email: string;
+  picUrl: string;
+  following: Array<string>;
+  recipes: Array<string>;
+  wishlist: Array<string>;
+  shoppinglist: Array<string>;
+  constructor(
+    username: string,
+    displayName: string,
+    email: string,
+    picUrl: string,
+    following: Array<string>,
+    recipes: Array<string>,
+    wishlist: Array<string>,
+    shoppinglist: Array<string>
+  ) {
+    this.username = username;
+    this.displayName = displayName;
+    this.email = email;
+    this.picUrl = picUrl;
+    this.following = following;
+    this.recipes = recipes;
+    this.wishlist = wishlist;
+    this.shoppinglist = shoppinglist;
+  }
+}
 
 @Component({
   selector: 'app-view-profiles',
@@ -6,10 +44,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-profiles.component.scss']
 })
 export class ViewProfilesComponent implements OnInit {
+  loggedIn = false;
+  userCollection: AngularFirestoreCollection<User>;
+  users: Observable<User[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public fAuth: AngularFireAuth,
+    private router: Router,
+    private afs: AngularFirestore
+  ) {
+    this.userCollection = this.afs.collection('users');
+    this.users = this.userCollection.valueChanges();
+    this.fAuth.onAuthStateChanged(auth => {
+      if (auth) {
+        this.loggedIn = true;
+      }
+      if (!auth) {
+        this.loggedIn = false;
+      }
+    });
   }
 
+  goToUser(username: string){
+    console.log(username);
+  }
+
+  ngOnInit() {}
 }
