bc0263c60d809be56a6a216cd859257446847977
Oliver Lance <olly.lance15@gmail.com>
2020-07-09 12:02:49 -0400

    update profile menu to link with auth - allow signin/logout

--- angularSTEP/src/app/home/home.component.ts
+++ angularSTEP/src/app/home/home.component.ts
@@ -5,20 +5,10 @@ import {
 import {Component, OnInit} from '@angular/core';
 
 import {AngularFireAuth} from '@angular/fire/auth';
+import {Converter} from '../converter';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
-
-//basic home page
-interface User {
-  username: string;
-  displayName: string;
-  email: string;
-  photo: string;
-  following: Array<string>;
-  recipes: Array<string>;
-  wishlist: Array<string>;
-  shoppinglist: Array<string>;
-}
+import {User} from '../user';
 
 @Component({
   selector: 'app-home',
@@ -26,9 +16,12 @@ interface User {
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  name = '';
   userCollection: AngularFirestoreCollection<User>;
   users: Observable<User[]>;
+  username = '';
+  displayName = '';
+  picUrl = 'assets/images/blank-profile.png';
+  updateData = false;
 
   constructor(
     public fAuth: AngularFireAuth,
@@ -39,7 +32,8 @@ export class HomeComponent implements OnInit {
     this.users = this.userCollection.valueChanges();
     this.fAuth.onAuthStateChanged(auth => {
       if (auth) {
-        this.name = auth.displayName !== null ? auth.displayName : '';
+        this.username = auth.displayName !== null ? auth.displayName : '';
+        this.setUserData();
       }
       if (!auth) {
         this.router.navigate(['/login']);
@@ -47,10 +41,32 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  ngOnInit() {}
+
   logout() {
     this.fAuth.signOut();
     this.router.navigate(['/login']);
   }
 
-  ngOnInit() {}
+  //this method sets the users data and then injects it
+  //into the profile menu dropdown. It waits until all of the
+  //information is set and then displays the menu.
+  async setUserData() {
+    const postUser = await this.afs
+      .doc('/users/' + this.username + '/')
+      .ref.withConverter(new Converter().converter)
+      .get();
+    if (postUser !== null && postUser.data() !== undefined) {
+      const user: User = postUser.data()!;
+      this.displayName = user.displayName !== null ? user.displayName : '';
+      this.picUrl =
+        user.picUrl !== null && user.picUrl !== ''
+          ? user.picUrl
+          : 'assets/images/blank-profile.png';
+      this.updateData = true;
+    } else {
+      this.fAuth.signOut();
+      this.router.navigate(['/login']);
+    }
+  }
 }
--- angularSTEP/src/app/profile-menu/profile-menu.component.ts
+++ angularSTEP/src/app/profile-menu/profile-menu.component.ts
@@ -1,33 +1,50 @@
 import {Component, Input, OnInit} from '@angular/core';
 
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Router} from '@angular/router';
+
+interface UserData {
+  displayName: string;
+  username: string;
+  picUrl: string;
+}
+
 @Component({
   selector: 'app-profile-menu',
   templateUrl: './profile-menu.component.html',
   styleUrls: ['./profile-menu.component.scss'],
 })
 export class ProfileMenuComponent implements OnInit {
-  @Input() user: any;
+  @Input() user!: UserData;
   username = '';
   displayName = '';
   picUrl = 'assets/images/blank-profile.png';
   //fixed value for now - implement input to show if user is actually signed in
-  loggedIn = false;
+  loggedIn = true;
 
-  constructor() {}
+  constructor(public fAuth: AngularFireAuth, public router: Router) {
+    this.fAuth.onAuthStateChanged(auth => {
+      if (auth) {
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.displayName = this.user.displayName;
     this.username = this.user.username;
     this.picUrl = this.user.picUrl;
-    this.loggedIn = this.user.loggedIn;
   }
 
   //temporary logout and signin functions
   logout() {
-    this.loggedIn = !this.loggedIn;
+    this.fAuth.signOut();
+    this.loggedIn = false;
   }
 
   signIn() {
-    this.loggedIn = !this.loggedIn;
+    this.router.navigate(['/signin']);
   }
 }
