68b3418f3b85e0d8b18cc9d5e68dd7a7abb0343b
Oliver Lance <olly.lance15@gmail.com>
2020-07-15 09:38:06 -0400

    add image upload form

--- angularSTEP/src/app/change-profile-img/change-profile-img.component.ts
+++ angularSTEP/src/app/change-profile-img/change-profile-img.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
 @Component({
   selector: 'app-change-profile-img',
   templateUrl: './change-profile-img.component.html',
-  styleUrls: ['./change-profile-img.component.scss']
+  styleUrls: ['./change-profile-img.component.scss'],
 })
 export class ChangeProfileImgComponent implements OnInit {
+  error = '';
+  fileForm: FormGroup;
+  hide = true;
+  imgFile!: object;
+  pictureUrl = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public fAuth: AngularFireAuth, private fb: FormBuilder) {
+    this.fileForm = this.fb.group({
+      image: [null, [requiredFileType]],
+    });
   }
 
+  logout() {
+    this.fAuth.signOut();
+  }
+
+  ngOnInit() {}
+
+  onFileSelect(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.imgFile = file;
+    }
+  }
+
+  onFileSubmit() {
+    //add image to database
+  }
+}
+
+function requiredFileType(control: FormControl) {
+  const imageTypes: Array<string> = ['png', 'jpg', 'jpeg', 'gif'];
+  const file = control.value;
+  if (file !== null) {
+    const type = file.split('.')[1].toLowerCase();
+    if (imageTypes.indexOf(type) > -1) {
+      return null;
+    }
+  }
+  return {fileType: true};
 }
