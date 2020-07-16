578afbc0422cb97ed8c52651d3237e3cac06f2c4
Oliver Lance <olly.lance15@gmail.com>
2020-07-16 08:45:29 -0400

    add zoning to detect changes & change profile css

--- angularSTEP/src/app/current-profile-page/current-profile-page.component.ts
+++ angularSTEP/src/app/current-profile-page/current-profile-page.component.ts
@@ -23,14 +23,14 @@ export class CurrentProfilePageComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private afs: AngularFirestore,
-    private _ngZone: NgZone
+    private zone: NgZone
   ) {
     this.fAuth.onAuthStateChanged(auth => {
       if (auth) {
         this.uid = auth.uid;
         this.setUserData();
       } else {
-        this._ngZone.run(() => {
+        this.zone.run(() => {
           this.router.navigate(['/login']);
         });
       }
@@ -62,7 +62,9 @@ export class CurrentProfilePageComponent implements OnInit {
           }
         }
       });
-      this.user = postUser.data()!;
+      this.zone.run(() => {
+        this.user = postUser.data()!;
+      })
     } else {
       this.router.navigate(['/login']);
     }
