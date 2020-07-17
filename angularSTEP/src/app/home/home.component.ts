1607e833cff095d3efbf45892bf65d77891950f5
Oliver Lance <olly.lance15@gmail.com>
2020-07-17 11:24:17 -0400

    full functionality to map

--- angularSTEP/src/app/home/home.component.ts
+++ angularSTEP/src/app/home/home.component.ts
@@ -6,80 +6,8 @@ import {Component, OnInit, ViewChild, ElementRef, NgZone} from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('mapElem', {static: true}) mapElem!: ElementRef;
-  address = '';
-  center: google.maps.LatLngLiteral = {lat: 40.157318, lng: -76.3069};
-  zoom = 13;
-  options: google.maps.MapOptions = {
-    center: this.center,
-    zoom: this.zoom,
-    zoomControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-    maxZoom: 15,
-    minZoom: 8,
-  };
-  markers = [];
-  infoContent = '';
-
-  map!: google.maps.Map;
-  service!: google.maps.places.PlacesService;
-  infowindow!: google.maps.InfoWindow;
-  request!: google.maps.places.PlaceSearchRequest;
+  
   constructor(private zone: NgZone) {}
-
-  //sets the map
-  ngAfterViewInit() {
-    this.zone.run(() => {
-      this.map = new google.maps.Map(this.mapElem.nativeElement, this.options);
-      this.getResults();
-    });
-  }
-
-  getResults() {
-    this.request = {
-      location: this.center,
-      radius: 16093,
-      rankBy: google.maps.places.RankBy.PROMINENCE,
-      type: 'supermarket',
-    };
-    this.service = new google.maps.places.PlacesService(this.map);
-    this.service.nearbySearch(this.request, this.callback);
-  }
-
-  callback(
-    results: google.maps.places.PlaceResult[],
-    status: google.maps.places.PlacesServiceStatus
-  ) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (let i = 0; i < results.length; i++) {
-        console.log(results[i]);
-      }
-    }
-  }
-
-  getNewCenter(place: google.maps.places.PlaceResult) {
-    this.center = {
-      lat: place.geometry?.location.lat() ? place.geometry?.location.lat() : 0,
-      lng: place.geometry?.location.lng() ? place.geometry?.location.lng() : 0,
-    };
-    this.map.setCenter(this.center);
-    this.getResults();
-  }
-
-  zoomIn() {
-    if (this.zoom < this.options.maxZoom!) {
-      this.zoom++;
-    }
-    this.map.setZoom(this.zoom);
-  }
-
-  zoomOut() {
-    if (this.zoom > this.options.minZoom!) {
-      this.zoom--;
-    }
-    this.map.setZoom(this.zoom);
-  }
 
   ngOnInit() {}
 }
--- angularSTEP/src/app/map/map.component.ts
+++ angularSTEP/src/app/map/map.component.ts
@@ -1,10 +1,10 @@
 import {
   Component,
   ViewChild,
-  EventEmitter,
-  Output,
+  ElementRef,
   OnInit,
   Input,
+  NgZone
 } from '@angular/core';
 import {FormGroup, FormBuilder} from '@angular/forms';
 
@@ -15,24 +15,45 @@ import {FormGroup, FormBuilder} from '@angular/forms';
 })
 export class MapComponent implements OnInit {
   @Input() addressType!: string;
-  @Output() setAddress: EventEmitter<
-    google.maps.places.PlaceResult
-  > = new EventEmitter();
   @ViewChild('addresstext') addresstext: any;
 
   mapsForm: FormGroup;
   autocompleteInput!: string;
 
-  constructor(private fb: FormBuilder) {
+  @ViewChild('mapElem', {static: true}) mapElem!: ElementRef;
+  address = '';
+  center: google.maps.LatLngLiteral = {lat: 39.068229, lng: -94.393225};
+  zoom = 11;
+  options: google.maps.MapOptions = {
+    center: this.center,
+    zoom: this.zoom,
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 3,
+  };
+  markers: google.maps.Marker[] = new Array();
+  infocontent: string = '';
+  map!: google.maps.Map;
+  service!: google.maps.places.PlacesService;
+  infowindow!: google.maps.InfoWindow;
+  request!: google.maps.places.PlaceSearchRequest;
+
+  constructor(private fb: FormBuilder, private zone: NgZone) {
     this.mapsForm = this.fb.group({
       location: [''],
     });
   }
 
-  ngOnInit(): void {}
-
-  //gives input the Google autocomplete. If value is entered, emits that value up to home
+  //Sets up the map and sets its center to the middle of the US. If value is entered, changes the zoom and
+  //center of map and then retrieves the results
   ngAfterViewInit() {
+    this.zone.run(() => {
+      this.map = new google.maps.Map(this.mapElem.nativeElement, this.options);
+      this.infowindow = new google.maps.InfoWindow();
+      this.map.setZoom(3);
+    });
     const autocomplete = new google.maps.places.Autocomplete(
       this.addresstext.nativeElement,
       {
@@ -41,7 +62,91 @@ export class MapComponent implements OnInit {
     );
     google.maps.event.addListener(autocomplete, 'place_changed', () => {
       const place = autocomplete.getPlace();
-      this.setAddress.emit(place);
+      this.center = {
+        lat: place.geometry?.location.lat() ? place.geometry?.location.lat() : 0,
+        lng: place.geometry?.location.lng() ? place.geometry?.location.lng() : 0,
+      };
+      this.zone.run(() => {
+        this.clearMarkers();
+        this.zoom = 11;
+        this.map.setZoom(this.zoom);
+        this.map.setCenter(this.center);
+        this.getResults();
+      })
     });
   }
+
+  ngOnInit(): void {}
+
+  //retreives the results 10 miles around the given location and sets up listener for infomarker
+  getResults() {
+    this.request = {
+      location: this.center,
+      radius: 16093,
+      rankBy: google.maps.places.RankBy.PROMINENCE,
+      type: 'grocery_or_supermarket',
+    };
+    this.service = new google.maps.places.PlacesService(this.map);
+    this.service.nearbySearch(this.request, (results, status) => this.zone.run(() => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (let i = 0; i < results.length; i++) {
+            const marker = new google.maps.Marker({
+              map: this.map,
+              position: results[i].geometry!.location,
+              title: results[i].name
+            });
+
+            google.maps.event.addListener(marker, "click", () => {
+              const requestInfo: google.maps.places.PlaceDetailsRequest = {
+                placeId: results[i].place_id!,
+                fields: ['formatted_phone_number', 'vicinity', 'website']
+              }
+              this.service.getDetails(requestInfo, (res, stat) => this.zone.run(() => {
+                if (stat === google.maps.places.PlacesServiceStatus.OK) {
+                const infocontent = '<div id="content">' +
+                '<div id="siteNotice">' +
+                "</div>" +
+                '<h1 id="firstHeading" class="firstHeading">'+ results[i].name +'</h1>' +
+                '<div id="bodyContent">' +
+                "<p>Phone Number: " + res.formatted_phone_number +
+                "<br/>Address: " + res.vicinity +
+                "<br/>Rating: " + results[i].rating + "/5"+
+                "<br><a href=" + res.website+" target='_blank'>Visit Website</a>"+
+                "</p>" +
+                "</div>" +
+                "</div>";
+                this.infowindow.setContent(infocontent);
+                this.infowindow.open(this.map, marker);
+                }
+              })) 
+            });
+            this.markers.push(marker);
+          }
+        }
+    }))
+  }
+
+  //clears markers if user changes to different location
+  clearMarkers(){
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(null);
+    }
+    this.markers = [];
+  }
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom!) {
+      this.zoom++;
+    }
+    this.map.setZoom(this.zoom);
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom!) {
+      this.zoom--;
+    }
+    this.map.setZoom(this.zoom);
+  }
+
+  
 }
