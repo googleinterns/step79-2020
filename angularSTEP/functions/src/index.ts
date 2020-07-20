5563ce84337d132305f34b617757f59432b97f37
Oliver Lance <olly.lance15@gmail.com>
2020-07-20 15:25:39 -0400

    fix error in firebase functions

--- angularSTEP/functions/src/index.ts
+++ angularSTEP/functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin'
-import * as algoliasearch from 'algoliasearch'
+import algoliasearch from 'algoliasearch'
 
 admin.initializeApp();
 const env = functions.config();
