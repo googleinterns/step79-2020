ba80ff932b87457440d65643fb903ba9125fda09
Oliver Lance <olly.lance15@gmail.com>
2020-07-20 14:39:56 -0400

    add firebase functions for algolia

--- angularSTEP/functions/src/index.ts
+++ angularSTEP/functions/src/index.ts
@@ -1,8 +1,28 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+import * as algoliasearch from 'algoliasearch'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const env = functions.config();
+
+const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
+const index = client.initIndex("user_search");
+
+exports.indexUser = functions.firestore.document('users/{userId}').onCreate((snap, context) => {
+    const user = snap.data();
+    user.objectID = context.params.userId;
+    return index.saveObject(user);
+});
+
+exports.updateUser = functions.firestore.document('users/{userId}').onUpdate((snap, context) => {
+    const user = snap.after.data();
+    const object = Object.assign({ objectID: context.params.userId }, user);
+    return index.partialUpdateObject(object);
+});
+
+exports.unindexUser = functions.firestore
+    .document('users/{userId}')
+    .onDelete((snap, context) => {
+    const objectId = snap.id;
+    return index.deleteObject(objectId);
+});
