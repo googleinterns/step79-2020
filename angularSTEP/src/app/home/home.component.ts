50ba34379713ba4c8b659832ebdc948c1b64ad26
Oliver Lance <olly.lance15@gmail.com>
2020-07-23 09:32:55 -0400

    add in basic wiget loading

--- angularSTEP/src/app/home/home.component.ts
+++ angularSTEP/src/app/home/home.component.ts
@@ -1,4 +1,10 @@
-import {Component, OnInit, ViewChild, ElementRef, NgZone} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/firestore';
+import {User} from '../user'
+import {Converter} from '../converter'
 
 @Component({
   selector: 'app-home',
@@ -7,7 +13,26 @@ import {Component, OnInit, ViewChild, ElementRef, NgZone} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {}
+  userCollection: AngularFirestoreCollection<User> = this.afs.collection('users');
+  newUsers: firebase.firestore.QueryDocumentSnapshot<User>[];
+  topRecipes: firebase.firestore.QueryDocumentSnapshot<User>[];
+  newRecipes: firebase.firestore.QueryDocumentSnapshot<User>[];
+
+  constructor(private afs: AngularFirestore) {
+    this.userCollection.ref.orderBy("timestamp", "desc").limit(4).withConverter(new Converter().userConverter).get().
+        then((users) => {
+          this.newUsers = users.docs;
+        });
+    //temporary until recipe card gets made.
+    this.userCollection.ref.orderBy("timestamp", "asc").limit(4).withConverter(new Converter().userConverter).get().
+        then((users) => {
+          this.topRecipes = users.docs;
+        });
+    this.userCollection.ref.orderBy("username", "desc").limit(4).withConverter(new Converter().userConverter).get().
+        then((users) => {
+          this.newRecipes = users.docs;
+        });
+  }
 
   ngOnInit() {}
 }
