7cd3d1309cfd8e213d7c1136802e21a7e152d373
Oliver Lance <olly.lance15@gmail.com>
2020-07-23 09:02:52 -0400

    fix style

--- angularSTEP/src/app/discover-display/discover-display.component.ts
+++ angularSTEP/src/app/discover-display/discover-display.component.ts
@@ -3,45 +3,43 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/firestore';
-import {User} from '../user'
-import {Converter} from '../converter'
+import {User} from '../user';
+import {Converter} from '../converter';
 
 @Component({
   selector: 'app-discover-display',
   templateUrl: './discover-display.component.html',
-  styleUrls: ['./discover-display.component.scss']
+  styleUrls: ['./discover-display.component.scss'],
 })
 export class DiscoverDisplayComponent implements OnInit {
-  // recipeCollection: AngularFirestoreCollection<Recipe>;
-  // recipes: Observable<Recipe[]>;
-  userCollection: AngularFirestoreCollection<User> = this.afs.collection('users');
+  userCollection: AngularFirestoreCollection<User> = this.afs.collection(
+    'users'
+  );
   users: firebase.firestore.QueryDocumentSnapshot<User>[];
   @Input() sortType: string;
   @Input() contentType: string;
   @Input() direction: string;
-  cols: number;
 
   constructor(private afs: AngularFirestore) {}
 
-  ngOnChanges(changes: SimpleChanges){
-    
+  ngOnChanges(changes: SimpleChanges) {
     if (changes && changes.sortType) {
       this.sortType = changes.sortType.currentValue;
-    } if (changes && changes.contentType){
+    }
+    if (changes && changes.contentType) {
       this.contentType = changes.contentType.currentValue;
-    } if (changes && changes.direction) {
+    }
+    if (changes && changes.direction) {
       this.direction = changes.direction.currentValue;
     }
     this.getCollectionData();
   }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
-  getCollectionData(){
-    if(this.sortType && this.contentType && this.direction !== undefined){
-      switch(this.contentType) {
+  getCollectionData() {
+    if (this.sortType && this.contentType && this.direction !== undefined) {
+      switch (this.contentType) {
         case 'Recipes': {
           this.getRecipes();
           break;
@@ -57,54 +55,71 @@ export class DiscoverDisplayComponent implements OnInit {
     }
   }
 
-  getRecipes(){}
+  getRecipes() {}
 
-  getUsers(){
-    switch(this.sortType){
+  getUsers() {
+    switch (this.sortType) {
       case 'Time Created': {
-        if(this.direction){
-          this.userCollection.ref.orderBy("timestamp", "asc").withConverter(new Converter().userConverter).get().
-          then((users) => {
-            this.users = users.docs;
-          });
+        if (this.direction) {
+          this.userCollection.ref
+            .orderBy('timestamp', 'asc')
+            .withConverter(new Converter().userConverter)
+            .get()
+            .then(users => {
+              this.users = users.docs;
+            });
         } else {
-          this.userCollection.ref.orderBy("timestamp", "desc").withConverter(new Converter().userConverter).get().
-          then((users) => {
-            this.users = users.docs;
-          });
+          this.userCollection.ref
+            .orderBy('timestamp', 'desc')
+            .withConverter(new Converter().userConverter)
+            .get()
+            .then(users => {
+              this.users = users.docs;
+            });
         }
         break;
       }
       case 'Number of Recipes': {
         //temporary until num of recipes are actually added
-        if(this.direction){
-          this.userCollection.ref.orderBy("displayName", "desc").withConverter(new Converter().userConverter).get().
-          then((users) => {
-            this.users = users.docs;
-          });
+        if (this.direction) {
+          this.userCollection.ref
+            .orderBy('displayName', 'desc')
+            .withConverter(new Converter().userConverter)
+            .get()
+            .then(users => {
+              this.users = users.docs;
+            });
         } else {
-          this.userCollection.ref.orderBy("displayName", "asc").withConverter(new Converter().userConverter).get().
-          then((users) => {
-            this.users = users.docs;
-          });
+          this.userCollection.ref
+            .orderBy('displayName', 'asc')
+            .withConverter(new Converter().userConverter)
+            .get()
+            .then(users => {
+              this.users = users.docs;
+            });
         }
         break;
       }
       case 'Name': {
-        if(this.direction){
-          this.userCollection.ref.orderBy("displayName", "desc").withConverter(new Converter().userConverter).get().
-          then((users) => {
-            this.users = users.docs;
-          });
+        if (this.direction) {
+          this.userCollection.ref
+            .orderBy('displayName', 'desc')
+            .withConverter(new Converter().userConverter)
+            .get()
+            .then(users => {
+              this.users = users.docs;
+            });
         } else {
-          this.userCollection.ref.orderBy("displayName", "asc").withConverter(new Converter().userConverter).get().
-          then((users) => {
-            this.users = users.docs;
-          });
+          this.userCollection.ref
+            .orderBy('displayName', 'asc')
+            .withConverter(new Converter().userConverter)
+            .get()
+            .then(users => {
+              this.users = users.docs;
+            });
         }
         break;
       }
     }
   }
-
 }
