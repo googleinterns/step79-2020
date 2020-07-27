54a75b4189913bf95d7f52259f05b466ebc67ddf
Oliver Lance <olly.lance15@gmail.com>
2020-07-27 07:28:16 -0400

    fix existing unit tests

--- angularSTEP/src/app/auto-complete/auto-complete.component.spec.ts
+++ angularSTEP/src/app/auto-complete/auto-complete.component.spec.ts
@@ -1,6 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AutoCompleteComponent } from './auto-complete.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {AutoCompleteComponent} from './auto-complete.component';
 
 describe('AutoCompleteComponent', () => {
   let component: AutoCompleteComponent;
@@ -8,7 +14,17 @@ describe('AutoCompleteComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AutoCompleteComponent ]
+      declarations: [ AutoCompleteComponent ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatAutocompleteModule,
+        MatIconModule,
+        MatInputModule
+      ]
     })
     .compileComponents();
   }));
