a801023fa65846ac0bc821fb4238666be67f7819
ducre <ducre@google.com>
2020-07-30 13:43:16 -0400

    Delet build file

--- angularSTEP/build/src/app/recipe-card/recipe-card.component.d.ts
+++
@@ -1,9 +0,0 @@
-interface recipeData {
-    recipeName: string;
-    description: string;
-    difficulty: string;
-}
-export declare class RecipeCardComponent {
-    recipe: recipeData;
-}
-export {};
