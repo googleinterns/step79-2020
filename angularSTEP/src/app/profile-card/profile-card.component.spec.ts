8fb40acf0fea48aa66e77f7c30e3d59d78ee24d6
ducre <ducre@google.com>
2020-08-03 13:26:33 -0400

    New test file

---
+++ angularSTEP/src/app/profile-card/profile-card.component.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
+import { DebugElement, Component } from '@angular/core';
+
+import { By } from '@angular/platform-browser';
+
+import { ProfileCardComponent } from './profile-card.component';
+
+describe('ProfileCardComponent', () => {
+    let component: ProfileCardComponent;
+    let fixture: ComponentFixture<ProfileCardComponent>;
+    let de: DebugElement;
+
+    beforeEach(async(() =>{
+        TestBed.configureTestingModule({
+            declarations: [ ProfileCardComponent ],
+
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProfileCardComponent);
+        component = fixture.componentInstance;
+        de = fixture.debugElement;
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should be a string', () =>{
+        expect(component.displayName).toBeInstanceOf(String);
+    });
+});
