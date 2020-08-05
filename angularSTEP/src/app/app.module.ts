7131dc4fbc835fe5c1b897d146d19735e5e3be2c
Oliver Lance <olly.lance15@gmail.com>
2020-08-05 07:25:46 -0400

    merge main

--- angularSTEP/src/app/app.module.ts
+++ angularSTEP/src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatChipsModule } from '@angular/material/chips';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatChipsModule} from '@angular/material/chips';
 
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
@@ -11,50 +11,52 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AutoCompleteComponent} from './auto-complete/auto-complete.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ChangeProfileImgComponent} from './change-profile-img/change-profile-img.component';
 import {CurrentProfilePageComponent} from './current-profile-page/current-profile-page.component';
 import {EmailComponent} from './email/email.component';
 import {environment} from '../environments/environment';
 import {GoogleMapsModule} from '@angular/google-maps';
 import {HomeComponent} from './home/home.component';
 import {HttpClientModule} from '@angular/common/http';
+import {ItemDialogComponent} from './item-dialog/item-dialog.component';
 import {LoginComponent} from './login/login.component';
-import {MapComponent} from './map/map.component';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MapComponent} from './map/map.component';
+import {MatDialogModule} from '@angular/material/dialog';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatRippleModule} from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatDialogModule} from '@angular/material/dialog';
 import {MyProfileTabComponent} from './my-profile-tab/my-profile-tab.component';
 import {NavbarComponent} from './navbar/navbar.component';
+import {NgAisModule} from 'angular-instantsearch';
 import {NgModule} from '@angular/core';
 import {ProfileCardComponent} from './profile-card/profile-card.component';
 import {ProfileMenuComponent} from './profile-menu/profile-menu.component';
+import {RecipeCardComponent} from './recipe-card/recipe-card.component';
 import {RecipePageComponent} from './recipe-page/recipe-page.component';
 import {RecipesComponent} from './recipes/recipes.component';
-import {SetupComponent} from './setup/setup.component';
-import {SignupComponent} from './signup/signup.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {UploadRecipeComponent} from './upload-recipe/upload-recipe.component';
-import {ChangeProfileImgComponent} from './change-profile-img/change-profile-img.component';
-import {RecipeCardComponent} from './recipe-card/recipe-card.component';
-import {UserPageComponent} from './user-page/user-page.component'; 
-import {NgAisModule} from 'angular-instantsearch';
 import {SearchBoxComponent} from './search-box/search-box.component';
+import {SetupComponent} from './setup/setup.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {SignupComponent} from './signup/signup.component';
+import {UploadRecipeComponent} from './upload-recipe/upload-recipe.component'; 
+import {UserPageComponent} from './user-page/user-page.component'; 
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list'
 import {ViewProfilesComponent} from './view-profiles/view-profiles.component'; 
-
 
 @NgModule({
   declarations: [
@@ -76,11 +78,15 @@ import {ViewProfilesComponent} from './view-profiles/view-profiles.component';
     RecipesComponent,
     RecipePageComponent,
     CurrentProfilePageComponent,
+    ChangeProfileImgComponent,
     SearchBoxComponent,
-    MyProfileTabComponent,
-    ChangeProfileImgComponent
+    ShoppingListComponent,
+    ItemDialogComponent,
+    MyProfileTabComponent
   ],
-
+  entryComponents: [
+    ItemDialogComponent,
+  ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
@@ -91,35 +97,33 @@ import {ViewProfilesComponent} from './view-profiles/view-profiles.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    GoogleMapsModule,
     HttpClientModule,
-    MatCardModule,
-    MatDividerModule,
-    MatFormFieldModule,
+    MatAutocompleteModule,
     MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatSidenavModule,
     MatInputModule,
     MatIconModule,
-    MatFormFieldModule,
-    MatAutocompleteModule,
-    ReactiveFormsModule,
+    MatInputModule,
     MatMenuModule,
     MatProgressBarModule,
     MatRippleModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
     MatSelectModule,
+    MatSnackBarModule,
     MatStepperModule,
-    MatToolbarModule,
-    ReactiveFormsModule,
-    GoogleMapsModule,
     MatTabsModule,
-    MatChipsModule,
-    MatExpansionModule,
-    MatChipsModule,
+    MatToolbarModule,
     NgAisModule.forRoot(),
     MatDialogModule,
-    MatListModule
+    MatListModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- angularSTEP/src/app/recipe-page/recipe-page.component.ts
+++ angularSTEP/src/app/recipe-page/recipe-page.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Recipe} from '../recipe';
 import {RecipeConverter} from '../recipe-converter';
 import {User} from '../user';
 import {Converter} from '../converter';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {ItemDialogComponent} from '../item-dialog/item-dialog.component';
 
 @Component({
   selector: 'app-recipe-page',
@@ -13,29 +17,129 @@ import {Converter} from '../converter';
 })
 export class RecipePageComponent {
   id: string | null = this.route.snapshot.paramMap.get('id');
-  recipe!: Recipe;
+  pageRecipe!: Recipe;
+  user!: User;
   uploader: User | null;
 
-  constructor(private db: AngularFirestore, private route: ActivatedRoute) {
+  constructor(
+    private db: AngularFirestore,
+    private route: ActivatedRoute,
+    private fAuth: AngularFireAuth,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {
     this.setRecipeData();
+    this.fAuth.currentUser.then(user => {
+      if (user) {
+        this.setUserData(user.uid);
+      }
+    });
   }
 
   async setRecipeData() {
     const postRecipe = await this.db
-      .collection('recipes')
-      .doc(this.id)
+      .doc('/recipes/' + this.id + '/')
       .ref.withConverter(new RecipeConverter().recipeConverter)
       .get();
     if (postRecipe && postRecipe.data()) {
-      this.recipe = postRecipe.data();
-      const postUser = await this.db
+      this.pageRecipe = postRecipe.data();
+      const postUploader = await this.db
         .collection('users')
-        .doc(this.recipe.uploaderUid)
+        .doc(this.pageRecipe.uploaderUid)
         .ref.withConverter(new Converter().userConverter)
         .get();
-      if (postUser && postUser.data()) {
-        this.uploader = postUser.data();
+      if (postUploader && postUploader.data()) {
+        this.uploader = postUploader.data();
       }
     }
+  }
+
+  async setUserData(uid: string) {
+    const postUser = await this.db
+      .doc('/users/' + uid + '/')
+      .ref.withConverter(new Converter().userConverter)
+      .get();
+    if (postUser.data()) {
+      this.user = postUser.data()!;
+    }
+  }
+
+  objToMap(obj: any): Map<string, number> {
+    const mp = new Map();
+    Object.keys(obj).forEach(k => {
+      mp.set(k, obj[k]);
+    });
+    return mp;
+  }
+
+  addItem(item: string) {
+    const currentShoppingList: Map<string, number> = this.objToMap(
+      this.user.shoppingList
+    );
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.height = 'auto';
+    dialogConfig.width = 'auto';
+    dialogConfig.data = {
+      selectedItem: item,
+      add: true,
+      remove: false,
+      max: 0,
+    };
+
+    const dialogRef = this.dialog.open(ItemDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        if (currentShoppingList.has(item)) {
+          const currentValue = currentShoppingList.get(item);
+          if (currentValue) {
+            currentShoppingList.set(item, currentValue + result);
+          }
+        } else {
+          currentShoppingList.set(item, result);
+        }
+
+        const objectCurrentShoppingList = Array.from(
+          currentShoppingList
+        ).reduce(
+          (objectCurrentShoppingList, [key, value]) =>
+            Object.assign(objectCurrentShoppingList, {[key]: value}),
+          {}
+        );
+
+        this.fAuth.currentUser.then(user => {
+          if (user) {
+            this.db
+              .collection('users')
+              .doc(this.user.uid)
+              .ref.withConverter(new Converter().userConverter)
+              .update({shoppingList: objectCurrentShoppingList})
+              .then(() => {
+                this.setUserData(user.uid);
+                const snackBarRef = this.snackBar.open(
+                  'Item Added!',
+                  undefined,
+                  {
+                    duration: 1500,
+                  }
+                );
+              })
+              .catch(error => {
+                const snackBarRef = this.snackBar.open(
+                  'Something went wrong:' + error,
+                  undefined,
+                  {
+                    duration: 1500,
+                  }
+                );
+              });
+          }
+        });
+      }
+    });
   }
 }
